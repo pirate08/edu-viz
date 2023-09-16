bd75643d33b67502e487c7571ec2d4d10adeb6be
Mayukh <mayukhdebgoswami82@gmail.com>
2023-09-16 10:21:10 +0530

    Modified Literacy...

--- components/partials/Literacy.jsx
+++ components/partials/Literacy.jsx
@@ -8,7 +8,7 @@ const Literacy = () => {
       <div className='mb-10'>
         <div className='flex justify-center items-center flex-wrap gap-3 mt-5 sm:mt-10'>
           {/* --First Image-- */}
-          <div className='sm:border-2 sm:px-4 sm:py-3 rounded-md'>
+          <div className='sm:border sm:px-4 sm:py-3 rounded-md'>
             <Image
               src='/literacy-hero1.avif'
               width={400}
@@ -18,7 +18,7 @@ const Literacy = () => {
             />
           </div>
           {/* --Second Image-- */}
-          <div className='sm:border-2 sm:px-4 sm:py-3 rounded-md'>
+          <div className='sm:border sm:px-4 sm:py-3 rounded-md'>
             <Image
               src='/literacy-hero3.avif'
               width={400}
@@ -30,7 +30,7 @@ const Literacy = () => {
         </div>
         <div className='flex justify-center items-center flex-wrap gap-3 mt-4'>
           {/* --Third Image-- */}
-          <div className='sm:border-2 sm:px-4 sm:py-3 rounded-md'>
+          <div className='sm:border sm:px-4 sm:py-3 rounded-md'>
             <Image
               src='/literacy-hero4.avif'
               width={400}
@@ -40,24 +40,12 @@ const Literacy = () => {
             />
           </div>
           {/* --Fourth Image-- */}
-          <div className='sm:border-2 sm:px-4 sm:py-3 rounded-md'>
+          <div className='sm:border sm:px-4 sm:py-3 rounded-md'>
             <Image
               src='/literacy-hero2.avif'
               width={400}
               height={100}
               alt='Hero-4'
-              className='rounded-md hover:scale-110 duration-300 cursor-pointer'
-            />
-          </div>
-        </div>
-        <div className='flex justify-center items-center flex-wrap gap-3 mt-4'>
-          {/* --Fifth Image-- */}
-          <div className='sm:border-2 sm:px-4 sm:py-3 rounded-md sm:mt-3'>
-            <Image
-              src='/literacy-hero5.avif'
-              width={800}
-              height={100}
-              alt='Hero-5'
               className='rounded-md hover:scale-110 duration-300 cursor-pointer'
             />
           </div>
@@ -154,6 +142,91 @@ const Literacy = () => {
             Literacy promotes global citizenship by facilitating cross-cultural
             understanding and encouraging individuals to engage with global
             issues and perspectives.
+          </li>
+        </ol>
+      </div>
+      <div className='flex justify-center items-center flex-wrap gap-3 mt-4 mb-8'>
+        {/* --Fifth Image-- */}
+        <div className='sm:border sm:px-4 sm:py-3 rounded-md sm:mt-3'>
+          <Image
+            src='/literacy-hero5.avif'
+            width={700}
+            height={100}
+            alt='Hero-5'
+            className='rounded-md hover:scale-110 duration-300 cursor-pointer'
+          />
+        </div>
+      </div>
+      {/* --Why Literacy Matters-- */}
+      <div className='mb-7'>
+        <h1 className='text-xl md:text-2xl font-bold text-blue-900'>
+          Why Literacy Matters
+        </h1>
+        <p className='text-gray-600 mt-4 text-md md:text-lg'>
+          Literacy is not just about reading and writing; it's a fundamental
+          skill that empowers individuals and contributes to the development and
+          progress of societies. Here are compelling reasons why literacy is
+          crucial:
+        </p>
+        <ol className='list-decimal pl-4'>
+          <li className='text-gray-600 mt-4 text-md md:text-lg'>
+            <span className='text-green-700 font-bold'>
+              Access to Education:{' '}
+            </span>
+            Literacy is the gateway to education. Without basic literacy skills,
+            individuals struggle to access formal education, limiting their
+            opportunities for personal growth and advancement.
+          </li>
+          <li className='text-gray-600 mt-4 text-md md:text-lg'>
+            <span className='text-green-700 font-bold'>Empowerment: </span>
+            Literacy empowers individuals to make informed choices, participate
+            in decision-making processes, and advocate for their rights. It
+            gives them a voice in their communities and society at large.
+          </li>
+          <li className='text-gray-600 mt-4 text-md md:text-lg'>
+            <span className='text-green-700 font-bold'>Reducing Poverty: </span>
+            Literacy is a powerful tool for poverty reduction. It opens doors to
+            better job opportunities and economic stability, allowing
+            individuals to provide for themselves and their families.
+          </li>
+          <li className='text-gray-600 mt-4 text-md md:text-lg'>
+            <span className='text-green-700 font-bold'>
+              Civic Participation:{' '}
+            </span>
+            In democratic societies, literacy is essential for citizens to
+            engage in civic activities, including voting, understanding
+            policies, and holding governments accountable.
+          </li>
+          <li className='text-gray-600 mt-4 text-md md:text-lg'>
+            <span className='text-green-700 font-bold'>
+              Global Citizenship:{' '}
+            </span>
+            In a globalized world, literacy is essential for understanding
+            different cultures, perspectives, and global issues. It fosters
+            empathy and a sense of global citizenship.
+          </li>
+          <li className='text-gray-600 mt-4 text-md md:text-lg'>
+            <span className='text-green-700 font-bold'>
+              Personal Development:{' '}
+            </span>
+            Literacy enhances personal development by fostering critical
+            thinking, problem-solving, and creativity. It broadens horizons and
+            enables lifelong learning.
+          </li>
+          <li className='text-gray-600 mt-4 text-md md:text-lg'>
+            <span className='text-green-700 font-bold'>
+              Workforce Readiness:{' '}
+            </span>
+            In the modern job market, literacy is a fundamental skill. It
+            enables individuals to adapt to changing work environments, learn
+            new technologies, and remain competitive in their careers.
+          </li>
+          <li className='text-gray-600 mt-4 text-md md:text-lg'>
+            <span className='text-green-700 font-bold'>
+              Innovation and Progress:{' '}
+            </span>
+            Literate societies are often more innovative and contribute to
+            scientific, technological, and cultural advancements.
           </li>
         </ol>
       </div>
